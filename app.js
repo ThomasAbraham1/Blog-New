d5567f8da28ead4389cd6fa1567a892c0c407969
ThomasAbraham1 <74496765+ThomasAbraham1@users.noreply.github.com>
2023-11-07 08:40:36 +0530

    monkey boi

--- app.js
+++ app.js
@@ -37,6 +37,10 @@ app.use(passport.session());
 
 mongoose.connect("mongodb://127.0.0.1:27017/blog", { useNewUrlParser: true });
 
+const postSchema = {
+  postTitle: String,
+  postContent: String,
+}
 
 
 const userSchema = new mongoose.Schema({
@@ -47,12 +51,12 @@ const userSchema = new mongoose.Schema({
   password: String,
   OAuthId: String,
   secretMsg: String,
-
+  posts: [postSchema],
 });
 
 // Plugins for mongodb
 
-userSchema.plugin(passportLocalMongoose, {usernameField: "email"});
+userSchema.plugin(passportLocalMongoose, { usernameField: "email" });
 userSchema.plugin(findOrCreate);
 
 const User = new mongoose.model('user', userSchema);
@@ -67,23 +71,6 @@ passport.deserializeUser(function (user, done) {
   done(null, user);
 });
 
-
-
-
-
-
-
-
-const postSchema = {
-  postTitle: String,
-  postContent: String,
-}
-
-const blogUsersSchema = {
-  name: String,
-  blogs: [postSchema],
-}
-
 const Post = new mongoose.model('Post', postSchema);
 
 const post = new Post({
@@ -96,8 +83,21 @@ const post = new Post({
 app.get('/', function (req, res) {
   console.log(req.isAuthenticated());
   // Selecting all the records from the database
-  Post.find({}).then(function (posts) {
-    res.render('home', { homeText: homeText, posts: posts });
+  User.find({ _id: req.user._id, username: { $eq: null } }, {
+    new: true
+  }).then((foundUser) => {
+    if (foundUser.length == 1) {
+      // console.log("hello");
+      User.findOne({ _id: req.user._id }).then(function (user) {
+        res.render('home', { homeText: homeText, posts: user.posts, isUserNameSet: "0" });
+      });
+    } else {
+      // console.log(req.session);
+      User.findOne({ _id: req.user._id }).then(function (user) {
+        // console.log(user.posts);
+        res.render('home', { homeText: homeText, posts: user.posts, isUserNameSet: "1", userName: req.user.username });
+      });
+    }
   });
 });
 
@@ -105,12 +105,26 @@ app.post('/', function (req, res) {
   var postTitle = req.body.postTitle;
   var postContent = req.body.postContent;
   // Storing the post values into database variables
-  const post = new Post({
-    postTitle: postTitle,
-    postContent: postContent,
+
+  User.findOne({ _id: req.user._id }).then((user) => {
+    user.posts.push({
+      postTitle: postTitle,
+      postContent: postContent,
+    });
+    // var userObject = user.toObject();
+    // delete 
+    user.save();
   });
   // Saving into the database
   post.save().then(function () {
+    res.redirect('/');
+  });
+});
+
+app.post('/userUpdation', (req, res) => {
+  User.findOneAndUpdate({ _id: req.user._id }, { username: req.body.userName }, { new: true }).then((resultRecord) => {
+    console.log('Updated userName of the new user \n' + resultRecord);
+    req.session.passport.user.username = req.body.userName;
     res.redirect('/');
   });
 });
@@ -122,8 +136,9 @@ app.get('/compose', function (req, res) {
 
 app.get("/posts/:post", function (req, res) {
   var postTitle = req.params.post;
-  Post.findOne({ postTitle: postTitle }).then(function (post) {
-    res.render('post.ejs', { postTitle: post.postTitle, postContent: post.postContent });
+  User.findOne({_id: req.user._id}, {posts: {$elemMatch: {postTitle: postTitle}}}).then(function (post) {
+    // console.log(post);
+    res.render('post.ejs', { postTitle: post.posts[0].postTitle, postContent: post.posts[0].postContent });
   });
 });
 
@@ -137,9 +152,9 @@ app.get('/login', function (req, res) {
 
 app.get('/logout', (req, res) => {
   req.logout((err) => {
-      if (err) { 
-          return err;
-      }
+    if (err) {
+      return err;
+    }
   });
   res.redirect('/');
 });
@@ -157,17 +172,14 @@ app.get('/register', function (req, res) {
 
 app.post('/register', (req, res) => {
   User.register({ email: (req.body.email) }, (req.body.password), function (err, user) {
-      if (err) {
-          console.log(err);
-          res.redirect('/register');
-      } else {
-        User.find({_id: req.user._id, username: {$eq: null}}).then( (foundUser) => {
-          console.log(foundUser);
-        } );
-          passport.authenticate('local')(req, res, function () {
-              res.redirect('/');
-          });
-      }
+    if (err) {
+      console.log(err);
+      res.redirect('/register');
+    } else {
+      passport.authenticate('local')(req, res, function () {
+        res.redirect('/');
+      });
+    }
   });
 
 });
